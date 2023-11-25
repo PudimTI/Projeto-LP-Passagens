2ed74321b5f9831d9ac42af7e2c3e7f426ba092f
João Pedro Vaz <pp.joao.mg@hotmail.com>
2023-11-25 12:58:32 -0300

    Atualização de codigo
    Adicionado as funções pendentes: Registrar Entrada e Registrar Sida
    
    Adicionado uma nova função: Cadastro de Passageiro

--- Projeto/main.c
+++ Projeto/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 const int MAX_ITEM = 20;
-int contPassageiro = 1;
+int contPassageiro = 0;
+
 
 /**
  * Elemento Struct
@@ -15,6 +16,8 @@ typedef struct {
     int estacaoEntrada;
     int estacaoSaida;
 } Passageiro;
+
+void cadastroPassageiro();
 
 void registrarEntrada(Passageiro* passageiros, int* totalPassageiros, int estacaoEntrada);
 
@@ -73,11 +76,84 @@ int main() {
 
 }
 
-void registrarEntrada(Passageiro* passageiros, int* totalPassageiros, int estacaoEntrada){
+void cadastroPassageiro(){
     Passageiro passageiro[MAX_ITEM];
     passageiro[contPassageiro].idCartao = contPassageiro;
-    printf("Digite o nome do passageiro\n");
-    scanf("%s", &passageiro[contPassageiro].nomePassageiro);
-    printf("Digite o tempo ")
+    printf("Digite o nome do passageiro: \n");
+    puts(passageiro[contPassageiro].nomePassageiro);
+    contPassageiro++;
+}
+
+void registrarEntrada(Passageiro* passageiros, int* totalPassageiros, int estacaoEntrada){
+    Passageiro passageiro[MAX_ITEM];
+    int idCartao = 0;
+    int estacao;
+
+    printf("Escolha o ID do passageiro que deseja: \n");
+    scanf("%d", &idCartao);
+
+    if (idCartao < 0 && idCartao > contPassageiro){
+        printf("ID não encontrado.\n");
+        printf("Cadastrando novo passageiro...\n");
+        cadastroPassageiro();
+        idCartao = contPassageiro;
+
+        return; //Para fazer: um metodo de perguntar no caso de criar um novo Passageiro ou digitar um novo numero
+    }else{
+        idCartao = contPassageiro;
+    }
+
+    struct tm tempoEntrada;
+
+    printf("\nInsira o horário de entrada (HH:MM:SS): ");
+    scanf("%d:%d:%d", &tempoEntrada.tm_hour, &tempoEntrada.tm_min, &tempoEntrada.tm_sec);
+
+    passageiro[idCartao].horarioEntrada = mktime(&tempoEntrada);
+
+    printf("\nInsira o numero da estação de entrada");
+    scanf("%d", &estacao);
+
+    /*
+     * Adicionar +1 no total de passageiros da estação escolhida.
+     *  provavelmente usar Structs para as estações disponiveis MAX 5
+    */
+}
+
+void registrarSaida(Passageiro* passageiros, int totalPassageiros, int estacaoSaida){
+    Passageiro passageiro[MAX_ITEM];
+    int idCartao = 0;
+    int estacao;
+
+    printf("Escolha o ID do passageiro que deseja: \n");
+    scanf("%d", &idCartao);
+
+    if (idCartao < 0 && idCartao > contPassageiro){
+        printf("ID não encontrado.\n");
+        printf("Cadastrando novo passageiro...\n");
+        cadastroPassageiro();
+        idCartao = contPassageiro;
+
+        return; //Para fazer: um metodo de perguntar no caso de criar um novo Passageiro ou digitar um novo numero
+    }else{
+        idCartao = contPassageiro;
+    }
+
+    struct tm tempoSaida;
+
+    printf("\nInsira o horário de saida (HH:MM:SS): ");
+    scanf("%d:%d:%d", &tempoSaida.tm_hour, &tempoSaida.tm_min, &tempoSaida.tm_sec);
+
+    time_t t1 = mktime(&tempoSaida);
+    time_t t2 = passageiro[idCartao].horarioEntrada;
+
+    if (t1 < t2){
+        printf("Tempo de saida invalido, tempo de saida antes de passageiro entrar\n");
+        return;
+    }
+
+    passageiro[idCartao].horarioSaida = t1;
+
+    printf("\nInsira o numero da estação de saida do metro.");
+    scanf("%d", &estacao);
 
 }
