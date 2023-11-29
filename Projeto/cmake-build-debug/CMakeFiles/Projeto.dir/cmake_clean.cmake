9cb7a0b2826c3a3ce0ced1dc50b63568f53b19ec
João Pedro Vaz <pp.joao.mg@hotmail.com>
2023-11-28 23:16:36 -0300

    Grande Atualização
    Feito o total de passageiros e outras logicas do algoritmo atualizadas, revizaremos o struct para o proximo comitt

---
+++ Projeto/cmake-build-debug/CMakeFiles/Projeto.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto.dir/main.c.obj"
+  "CMakeFiles/Projeto.dir/main.c.obj.d"
+  "Projeto.exe"
+  "Projeto.exe.manifest"
+  "Projeto.pdb"
+  "libProjeto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projeto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Projeto/main.c
+++ Projeto/main.c
@@ -2,6 +2,8 @@
 #include <time.h>
 const int MAX_ITEM = 20;
 int contPassageiro = 0;
+int totalPassageiros[5][2]; //Uma matriz para guardar a estação como posição (de 1 a 5) e a quantidade de passageiros, sendo a primeira posição as entradas e a segunda a saida
+
 
 
 /**
@@ -17,15 +19,17 @@ typedef struct {
     int estacaoSaida;
 } Passageiro;
 
+//Passageiro passageiros[MAX_ITEM];
+
 void cadastroPassageiro();
 
-void registrarEntrada(Passageiro* passageiros, int* totalPassageiros, int estacaoEntrada);
+void registrarEntrada(Passageiro* passageiros, int estacaoEntrada);
 
-void registrarSaida(Passageiro* passageiros, int totalPassageiros, int estacaoSaida);
+void registrarSaida(Passageiro* passageiros, int estacaoSaida);
 
-void gerarRelatorio(Passageiro* passageiros, int totalPassageiros, time_t inicioIntervalo, time_t fimIntervalo);
+void gerarRelatorio(Passageiro* passageiros, time_t inicioIntervalo, time_t fimIntervalo, const char *nomeArquivo);
 
-void visualizarFluxoEstacao(Passageiro* passageiros, int totalPassageiros, int estacao);
+void visualizarFluxoEstacao(Passageiro* passageiros, int estacao);
 
 int main() {
     /**
@@ -33,9 +37,18 @@ int main() {
      * deve conter
      */
     int opc = -1;
-    time_t inicioIntervalo, fimIntervalo;
     time_t time(time_t *inicioIntervalo);
     const char *nomeArquivo = "relatorio.txt";
+
+    struct tm intervaloI;
+    struct tm intervaloF;
+
+    //função para limpar totalPassageiros
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 2; j++) {
+            totalPassageiros[i][j] = 0;
+        }
+    }
 
     while (opc != 0){
         printf("*------ MENU ------*\n");
@@ -52,14 +65,30 @@ int main() {
 
         switch (opc) {
             case 1:
+                //registrar entrada
+
+
+                //registrarEntrada(passageiros[contPassageiros], estacao)
                 break;
             case 2:
+                //registrar saida
                 break;
             case 3:
+                //gerar relatorio
+                printf("Digite o inicio do intervalo que deseja gerar o relatorio \n");
+                scanf("%d:%d:%d", &intervaloI.tm_hour, &intervaloI.tm_min, &intervaloI.tm_sec);
 
+                printf("Digite o fim do intervalo que deseja gerar o relatorio \n");
+                scanf("%d:%d:%d", &intervaloF.tm_hour, &intervaloF.tm_min, &intervaloF.tm_sec);
+
+                time_t inicioIntervalo = mktime(&intervaloI);
+                time_t fimIntervalo = mktime(&intervaloF);
+
+                //gerarRelatorio(passageiros[idCartao],totalPassageiros ,inicioIntervalo, fimIntervalo);
 
                 break;
             case 4:
+                //fluxo de estação
                 break;
             case 0:
                 printf("\nSaindo...");
@@ -85,7 +114,7 @@ void cadastroPassageiro(){
     contPassageiro++;
 }
 
-void registrarEntrada(Passageiro* passageiros, int* totalPassageiros, int estacaoEntrada){
+void registrarEntrada(Passageiro* passageiros, int estacaoEntrada){
     Passageiro passageiro[MAX_ITEM];
     int idCartao = 0;
     int estacao;
@@ -118,9 +147,11 @@ void registrarEntrada(Passageiro* passageiros, int* totalPassageiros, int estaca
      * Adicionar +1 no total de passageiros da estação escolhida.
      *  provavelmente usar Structs para as estações disponiveis MAX 5
     */
+
+    totalPassageiros[estacao][0] = totalPassageiros[estacao][0] + 1;
 }
 
-void registrarSaida(Passageiro* passageiros, int totalPassageiros, int estacaoSaida){
+void registrarSaida(Passageiro* passageiros, int estacaoSaida){
     Passageiro passageiro[MAX_ITEM];
     int idCartao = 0;
     int estacao;
@@ -162,12 +193,15 @@ void registrarSaida(Passageiro* passageiros, int totalPassageiros, int estacaoSa
      *  provavelmente usar Structs para as estações disponiveis MAX 5
     */
 
+    totalPassageiros[estacao][1] = totalPassageiros[estacao][1] + 1;
+
 }
 
-void gerarRelatorio(Passageiro* passageiros, int totalPassageiros, time_t inicioIntervalo, time_t fimIntervalo, const char *nomeArquivo){
+void gerarRelatorio(Passageiro* passageiros, time_t inicioIntervalo, time_t fimIntervalo, const char *nomeArquivo){
     // Abrir o arquivo para escrita
     FILE *arquivo = fopen(nomeArquivo, "w");
     Passageiro passageiro[MAX_ITEM];
+
 
     // Verificar se o arquivo foi aberto com sucesso
     if (arquivo == NULL) {
@@ -179,12 +213,19 @@ void gerarRelatorio(Passageiro* passageiros, int totalPassageiros, time_t inicio
     fprintf(arquivo, "Relatório de Fluxo geral das estações\n");
     fprintf(arquivo, "--------------------------------------\n");
 
+    fprintf(arquivo, "Passageiros");
     // Escrever informações de cada funcionário no arquivo
     for (int i = 0; i < contPassageiro; i++) {
-        fprintf(arquivo, "ID do Cartão: %d\n", passageiro[i].idCartao);
-        fprintf(arquivo, "Nome: %d\n", passageiro[i].nomePassageiro);
-        fprintf(arquivo, ": %.d\n", );
-        fprintf(arquivo, "------------------------\n");
+        if (passageiro[i].horarioEntrada < inicioIntervalo && passageiro[i].horarioSaida > fimIntervalo){
+            fprintf(arquivo, "------------------------\n");
+            fprintf(arquivo, "ID do Cartão: %d\n", passageiro[i].idCartao);
+            fprintf(arquivo, "Nome: %d\n", passageiro[i].nomePassageiro);
+            fprintf(arquivo, "Horario de entrada: %.d\n", passageiro[i].horarioEntrada);
+            fprintf(arquivo, "Horario de saida: %.d\n", passageiro[i].horarioSaida);
+            fprintf(arquivo, "Estação de entrada: %.d\n", passageiro[i].estacaoEntrada);
+            fprintf(arquivo, "Estação de Saida: %.d\n", passageiro[i].estacaoSaida);
+            fprintf(arquivo, "------------------------\n");
+        }
     }
 
     
@@ -195,3 +236,6 @@ void gerarRelatorio(Passageiro* passageiros, int totalPassageiros, time_t inicio
     printf("Relatório gerado com sucesso e salvo em %s.\n", nomeArquivo);
 }
 
+void visualizarFluxoEstacao(Passageiro* passageiros, int estacao){
+
+}
