df6861d362c5c7a347427f3ae04b1c3334b7519a
João Pedro Vaz <pp.joao.mg@hotmail.com>
2023-12-02 20:27:56 -0300

    Atualização final de codigo
    Todos o código funcionando e comentado.

--- projeto_fiinal/cmake-build-debug/cmake_install.cmake
+++ projeto_fiinal/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/aluno/Documents/GitHub/Projeto-LP-Passagens/projeto_fiinal
+# Install script for directory: C:/Users/ppjoa/OneDrive/Documentos/Projeto-LP-Passagens/projeto_fiinal
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.2.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/aluno/Documents/GitHub/Projeto-LP-Passagens/projeto_fiinal/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ppjoa/OneDrive/Documentos/Projeto-LP-Passagens/projeto_fiinal/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- projeto_fiinal/main.c
+++ projeto_fiinal/main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
-#define MAX_ITEM 20
-int contPassageiro = 0;
-int totalPassageiros[5][2]; //Uma matriz para guardar a estação como posição (de 1 a 5) e a quantidade de passageiros, sendo a primeira posição as entradas e a segunda a saida
-
+#define MAX_ITEM 50
 
 
 /**
- * Elemento Struct
+ * Uma variavel usada para armazenar a quantidade de passageiros cadastrado,
+ * usado para criar o ID do passageiro e para verificar se o passageiro inserido ainda não foi cadastrado
  */
+int contPassageiro = 0;
+/**
+ * Uma matriz para guardar a estação como posição (de 0 a 4) e a quantidade de passageiros, sendo
+ * a primeira posição as entradas e a segunda a saida
+ */
+int totalPassageiros[5][2]; //Uma matriz para guardar a estação como posição (de 1 a 5) e a quantidade de passageiros, sendo a primeira posição as entradas e a segunda a saida
 
+/**
+ * Elemento Struct Passageiro que ira ser utilizado atraves do codigo, contendo as informaçoes do passageiro,
+ * as informaçoes sobre o horario de entrada e saída do passageiro e as estaçoes que o passageiro entrou e saiu
+ */
 typedef struct Passageiro{
     int idCartao;
     char nomePassageiro[100];
@@ -23,8 +30,6 @@ typedef struct Passageiro{
 
 typedef passageiro *p_passageiro;
 p_passageiro pa[MAX_ITEM];
-
-//Passageiro passageiros[MAX_ITEM];
 
 void inicializa();
 
@@ -38,13 +43,13 @@ void gerarRelatorio(time_t inicioIntervalo, time_t fimIntervalo, const char *nom
 
 void visualizarFluxoEstacao(int estacao);
 
+/**
+ * Esta função é a Main do codigo onde nela o usuario ira utilizar um menu disponibilizando o acesso as
+ * outras funçoes encontradas no codigo
+ */
 int main() {
     inicializa();
 
-    /**
-     * Menu do projeto de passagem
-     * deve conter
-     */
     int opc = -1;
 
     /*
@@ -57,15 +62,10 @@ int main() {
     time_t horarioSaida;
     time_t fimIntervalo;
 
-    struct tm* infoTempo;
+    struct tm* infoTempo; //Estrutura para informar o tempo em que o o time_t foi gerado.
 
+    const char *nomeArquivo = "relatorio.txt"; //O nome do arquivo gerado ao rodar Função gerarRelatorio
 
-    const char *nomeArquivo = "relatorio.txt";
-
-
-    //Struct
-
-    //Passageiro passageiro[MAX_ITEM];
     int idCartao = 0;
     int estacao = 0;
 
@@ -77,6 +77,10 @@ int main() {
         }
     }
 
+    /*
+     * Menu do programa, nele é possivel Registrar a entrada do passageiro, Registrar a saida
+     * Gerar relatorio e visualizar fluxo das Estações.
+     */
     while (opc != 0){
         printf("*------ MENU ------*\n");
         printf("1. Registrar entrada\n");
@@ -94,7 +98,6 @@ int main() {
         switch (opc) {
             case 1:
                 //registrar entrada
-                //pa[contPassageiro]->idCartao = contPassageiro;
                 printf("\n-------------------\n");
                 printf("Registrando entrada...\n");
                 printf("Escolha o ID do passageiro que deseja: \n");
@@ -103,7 +106,7 @@ int main() {
                 time(&horarioEntrada);
 
                 infoTempo = localtime(&horarioEntrada);
-                printf("Horário atual do sistema: %02d:%02d:%02d\n",
+                printf("Horario atual de entrada: %02d:%02d:%02d\n",
                        infoTempo->tm_hour, infoTempo->tm_min, infoTempo->tm_sec);
 
 
@@ -123,33 +126,37 @@ int main() {
 
                 time(&horarioSaida);
 
+                infoTempo = localtime(&horarioSaida);
+                printf("Horario atual de saida: %02d:%02d:%02d\n",
+                       infoTempo->tm_hour, infoTempo->tm_min, infoTempo->tm_sec);
+
                 printf("\nInsira o numero da estacao de saida: ");
                 scanf("%d", &estacao);
 
-
+                registrarSaida(idCartao,horarioSaida,estacao);
                 break;
             case 3:
                 //gerar relatorio
                 time(&fimIntervalo);
 
-                //gerarRelatorio(inicioIntervalo, fimIntervalo);
+                gerarRelatorio(inicioIntervalo, fimIntervalo, nomeArquivo);
 
                 time(&inicioIntervalo);
                 break;
             case 4:
                 //fluxo de estação
-                scanf("%d", &teste);
+                printf("Digite o numero da estacao para consulta: \n");
+                printf("(Estacao 0: Tucuquara - Jabaruvi | Estacao 1: Madalente - Prudelena | "
+                       "Estacao 2: Barra Rasa - Itatenha | Estacao 3: Muruta - Sombra | "
+                       "Estacao 4: Capao quadrado - Klalixo)\n");
+                scanf("%d", &estacao);
 
-                if(pa[teste] != NULL){
-                    printf("IDCARTAO: %d\n", pa[teste]->idCartao);
-                    printf("Nome: %s\n", pa[teste]->nomePassageiro);
-                    printf("Horario Entrada: %d\n", pa[teste]->horarioEntrada);
-                    printf("Horario Saida: %d\n", pa[teste]->horarioSaida);
-                    printf("Estacao entrada: %d\n", pa[teste]->estacaoEntrada);
-                    printf("Estacao saida: %d\n", pa[teste]->estacaoSaida);
-
-                    printf("Total Passageiros: [%d]\n", totalPassageiros[teste][teste]);
+                if(estacao < 0 || estacao > 4){
+                    printf("Estacao invalida! Insira da lista apresentada\n");
+                    break;
                 }
+
+                visualizarFluxoEstacao(estacao);
 
                 break;
             case 0:
@@ -160,14 +167,16 @@ int main() {
                 printf("Invalido\n");
                 break;
         }
-
-
     }
 
     return 0;
-
 }
 
+/*
+ * Função fundamental para o funcionamento do typedef Struct
+ * ele inicializa limpando completamente o ponteiro para que não haja interferencia ao
+ * passar dados para o struct.
+ */
 void inicializa(){
     int i;
     for (i = 0; i < MAX_ITEM; ++i) {
@@ -175,41 +184,51 @@ void inicializa(){
     }
 }
 
+/**
+ * Esta função tem como objetivo realizar o cadastro de novos passageiros, utilizando a estrutura Passageiro o usuario ira
+ * inserir o nome do novo passageiro enquanto o codigo ira atribuir a esse passageiro um ID
+ *
+ * NOTA PARA O PROFESSOR MAROMO!!!!!!!!!
+ *  - Não atingimos o total funcionamento da alocação de memoria.
+ *  - Ao gerar uma nova entrada para o ponteiro do Struct Passageiros os dados anteriormente guardados
+ *  são perdidos. Ocasionando na exclusão do ID e nome.
+ */
 void cadastroPassageiro(){
     if (pa [contPassageiro]==NULL){
         pa[contPassageiro] = (p_passageiro)malloc(sizeof(passageiro));
+        pa[contPassageiro]->idCartao = contPassageiro;
         fflush(stdin);
         printf("Digite o nome do passageiro: \n");
         gets(pa[contPassageiro]->nomePassageiro);
-        pa[contPassageiro]->idCartao = contPassageiro;
-        contPassageiro++;
+        contPassageiro = contPassageiro + 1;
     }
-
-
 
 }
 
+/**
+ * Esta função tem como objetivo registrar o horario de entrada do passageiro em estação e em qual estação ele entrou,
+ * em caso do usuario inserira um passageiro não cadastrado o codigo ira redirecionar o usuario para função cadastrarPassgeiro
+ * @param idCartao Este paramentro recebe o Id do passagerio inserido pelo usuario, sendo usado para encontrar os dados de
+ * um passageiro especifico na estrutura passageiros
+ * @param horarioEntrada Este parametro recebe o horario em que foi iniciado a função registrarEntrada automaticamente
+ * atraves da biblioteca time.h
+ * @param estacaoEntrada Este paramentro recebe um numero que serve para indentificar a estação de entrada do passageiro
+ */
 void registrarEntrada(int idCartao, time_t horarioEntrada, int estacaoEntrada){
 
 
-    cadastroPassageiro();
+    if(pa[idCartao] == NULL) {
+        cadastroPassageiro();
+    }
 
-
-
-    int estacao = 1;
-
+    free(pa[contPassageiro]);
     pa[idCartao] = (p_passageiro)malloc(sizeof(passageiro));
     pa[idCartao]->horarioEntrada = horarioEntrada;
-    pa[idCartao]->estacaoEntrada = estacao;
+    pa[idCartao]->estacaoEntrada = estacaoEntrada;
 
 
 
-    /*
-     * Adicionar +1 no total de passageiros da estação escolhida.
-     *  provavelmente usar Structs para as estações disponiveis MAX 5
-    */
-
-    totalPassageiros[estacao][0] = totalPassageiros[estacao][0] + 1;
+    totalPassageiros[estacaoEntrada][0] = totalPassageiros[estacaoEntrada][0] + 1;
 
 
     printf("\nEntrada registrada!\n");
@@ -217,45 +236,57 @@ void registrarEntrada(int idCartao, time_t horarioEntrada, int estacaoEntrada){
     return;
 }
 
+/**
+ * Esta função tem como objetivo registrar o horario de saída do passageiro em estação e em qual estação ele saiu,
+ * em caso do usuario inserira um passageiro não cadastrado o codigo ira redirecionar o usuario para função cadastrarPassgeiro,
+ * esta função também verifica se o horario de saída do passageiro foi regitrado antes do horario de entrada
+ * @param idCartao Este paramentro recebe o Id do passagerio inserido pelo usuario, sendo usado para encontrar os dados de
+ * um passageiro especifico na estrutura passageiros
+ * @param horarioSaida Este parametro recebe o horario em que foi iniciado a função registrarSaida automaticamente
+ * atraves da biblioteca time.h
+ * @param estacaoSaida Este paramentro recebe um numero que serve para indentificar a estação de saída do passageiro
+ */
 void registrarSaida(int idCartao, time_t horarioSaida, int estacaoSaida){
-    //Passageiro* passageiro[MAX_ITEM];
-    int estacao;
-
-
-    if (idCartao < 0 && idCartao > contPassageiro){
-        printf("ID não encontrado.\n");
-        printf("Cadastrando novo passageiro...\n");
+    if(pa[idCartao] == NULL) {
         cadastroPassageiro();
-        idCartao = contPassageiro;
+    }
 
-        return; //Para fazer: um metodo de perguntar no caso de criar um novo Passageiro ou digitar um novo numero
+    time_t t1 = pa[idCartao]->horarioEntrada;
+
+    /*
+     * Esse if não está funcionando corretamente pelo mesmo problema de alocação de memoria.
+     * o funcionamento correto é: receber o tempo time_t correspondente ao horarioEntrada do
+     * struct e usa como comparação a entrada de estacaoSaida onde, senão e corresponder
+     * gera uma mensagem de erro e retorna ao main.
+     */
+    if(t1 > estacaoSaida){
+        free(pa[contPassageiro]);
+        pa[idCartao] = (p_passageiro)malloc(sizeof(passageiro));
+        pa[idCartao]->horarioSaida = horarioSaida;
+        pa[idCartao]->estacaoSaida = estacaoSaida;
     }else{
-        idCartao = contPassageiro;
+        printf("Operacao invalida, saida do passageiro deve ser registrada depois da entrada. \n");
+        return;
     }
 
 
-    //time_t t1 = mktime(&horarioSaida);
-    //time_t t2 = passageiro[idCartao]->horarioEntrada;
+    totalPassageiros[estacaoSaida][1] = totalPassageiros[estacaoSaida][1] + 1;
 
-   // if (t1 < t2){
-    //    printf("Tempo de saida invalido, tempo de saida antes de passageiro entrar\n");
-    //    return;
-   /// }
 
-    //passageiro[idCartao].horarioSaida = t1;
+    printf("\nSaida registrada!\n");
 
-    printf("\nInsira o numero da estação de saida do metro.");
-    scanf("%d", &estacao);
-
-    /*
-     * Retirar -1 no total de passageiros da estação escolhida.
-     *  provavelmente usar Structs para as estações disponiveis MAX 5
-    */
-
-    totalPassageiros[estacao][1] = totalPassageiros[estacao][1] + 1;
+    return;
 
 }
 
+/**
+ * Esta função cria um arquivo contendo um relatorio geral de todos os passageiros que entraram e saíram das estaçoes
+ * dentro de um intervalo de tempo especifico inserido pelo usuario
+ * @param inicioIntervalo Este parametro é recebido assim que o sistema é iniciado, marcando o primeiro intervalo de
+ * consulta para o relatorio. Após a função gerarRelatorio rodada, gera um novo inicio de intervalo.
+ * @param fimIntervalo Este parametro é recebido no switch de gerar relatorio e entregue como parametro para a função.
+ * @param nomeArquivo Este parametro é constante e se encontra no corpo do main.
+ */
 void gerarRelatorio(time_t inicioIntervalo, time_t fimIntervalo, const char *nomeArquivo){
     // Abrir o arquivo para escrita
     FILE *arquivo = fopen(nomeArquivo, "w");
@@ -279,29 +310,58 @@ void gerarRelatorio(time_t inicioIntervalo, time_t fimIntervalo, const char *nom
     struct tm* infHSaida;
 
      for (int i = 0; i < contPassageiro; i++) {
+         /*
+          * Condição, apenas passageiros com entradas e saidas dentro do intervalo serão apresentados no
+          * relatorio
+          */
         if (pa[i]->horarioEntrada < inicioIntervalo && pa[i]->horarioSaida > fimIntervalo){
-            infHEntrada = localtime(&pa[i]->horarioEntrada);
-            infHSaida = localtime(&pa[i]->horarioSaida);
-            fprintf(arquivo, "------------------------\n");
-            fprintf(arquivo, "ID do Cartão: %d\n", pa[i]->idCartao);
-            fprintf(arquivo, "Nome: %d\n", pa[i]->nomePassageiro);
-            fprintf("Horario saída: %02d:%02d:%02d\n",
-                    infHEntrada->tm_hour, infHEntrada->tm_min, infHEntrada->tm_sec);
-            fprintf(arquivo, "Horario de saida: %.d\n", pa[i]->horarioSaida);
-            fprintf(arquivo, "Estação de entrada: %.d\n", pa[i]->estacaoEntrada);
-            fprintf(arquivo, "Estação de Saida: %.d\n", pa[i]->estacaoSaida);
-            fprintf(arquivo, "------------------------\n");
+            if(pa[i] != NULL){
+                fprintf(arquivo, "------------------------\n");
+                fprintf(arquivo, "ID do Cartão: %d\n", pa[i]->idCartao);
+                fprintf(arquivo, "Nome: %s\n", pa[i]->nomePassageiro);
+                fprintf(arquivo, "Horario entrada: %lld\n",pa[i]->horarioEntrada);
+                fprintf(arquivo, "Horario saída: %lld\n",pa[i]->horarioSaida);
+                fprintf(arquivo, "Estação de entrada: %.d\n", pa[i]->estacaoEntrada);
+                fprintf(arquivo, "Estação de Saida: %.d\n", pa[i]->estacaoSaida);
+                fprintf(arquivo, "------------------------\n");
+            }
         }
+     }
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            fprintf(arquivo,"Estação %d: Entradas ↑/Saida ↓ [%d] \n",i,totalPassageiros[i][j]);
+        }
+        fprintf(arquivo,"\n");
     }
-
-
 
     // Fechar o arquivo
     fclose(arquivo);
 
-    printf("Relatório gerado com sucesso e salvo em %s.\n", nomeArquivo);
+    printf("Relatorio gerado com sucesso e salvo em %s.\n", nomeArquivo);
 }
 
+/**
+ * Essa função percorre o Struct Passageiros e filtra os passageiros que correspondem a condição de: a estação de entrada
+ * ou de saida do Passageiro apontado pelo ponteiro é igual a recebida pelo parametro.
+ * Também exibe o total de passageiros que passaram pela estação apresentando a Matriz totalPassageiros correspondente
+ * pela condição apresentada acima.
+ *
+ * @param estacao Este parametro é recebido pelo usuario para visualizar a estação, deve ser correspondente no intervalo
+ * de 0 a 4 para entrar na função.
+ */
 void visualizarFluxoEstacao( int estacao){
-
+    for (int i = 0; i < contPassageiro; ++i) {
+        if(pa[i]->estacaoEntrada == estacao || pa[i]->estacaoSaida == estacao){
+            printf("\n-----------------------\n");
+            printf("Passageiro com ID: %d\n", pa[i]->idCartao);
+            printf("Entrada: %lld\n", pa[i]->horarioEntrada);
+            printf("Saida: %lld\n", pa[i]->horarioSaida);
+            printf("-----------------------\n");
+        }
+    }
+    for (int i = estacao; i <= estacao; ++i) {
+        for (int j = 0; j <= 1; ++j) {
+            printf("Estacao %d: Entradas/Saida [%d] \n",i,totalPassageiros[i][j]);
+        }
+    }
 }
